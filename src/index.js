c674328c2ee37799d2f7841e12c9f9c32af8c6be
Kaushal Kumar <sirkaushalkumar@gmail.com>
2019-03-19 08:54:16 -0700

    Add Project Files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './spinner'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    //constructor is particular to javascript and is called first when App is created.
+/*     constructor(props) {
+        super(props);
+        //This is the only time we do direct assignment to this.state
+        this.state = { lat: null, long: null, errorMessage: '' }
+    } */
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    state = {lat: null, long: null, errorMessage: '' }
+
+    componentDidMount() {
+        console.log('My component was rendered to the screen')
+        window.navigator.geolocation.getCurrentPosition(
+            (position) =>  this.setState({ lat: position.coords.latitude, long: position.coords.longitude }),
+                //We called setState to update
+                // we never do direct assignment. Its only done while assignment
+            //success callback
+            (err) => this.setState({ errorMessage: err.message })
+             //failure callback
+        )
+    }
+
+    componentDidUpdate() {
+        console.log('My coponent was just updated. It just rerendered.')
+    }
+
+    renderContent(){
+        if (this.state.errorMessage && !this.state.lat && !this.state.long) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat && this.state.long) {
+            return <SeasonDisplay lat={this.state.lat} long={this.state.long} />;
+
+        }
+        return <Spinner />;
+    }
+
+    //React says we have to define render
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        )
+        
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
